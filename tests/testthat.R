9e687b24d48eeb7a491ba8e777aa27c80a2ab111
Renaud <renozao@users.noreply.github.com>
2019-07-01 21:55:12 +0000

    #3 Added unit testing when RNGversion is set to 3.5.0

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,10 @@ library(testthat)
 library(rngtools)
 
 test_check("rngtools")
+
+# test that everything works fine when the RNG version is set on backward-compatibility mode
+if( utils::compareVersion(paste0(R.version$major, ".", R.version$minor), "3.6.0") >=  0 ){
+  RNGversion("3.5.0")
+  test_check("rngtools")
+  
+}
